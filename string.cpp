5d656a8a813f8fe0c268f65b39120af283601cf2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-10-04 17:21:09 +0530

    string

--- string.cpp
+++ string.cpp
@@ -2,9 +2,12 @@
 #include<cstring>
 using namespace std;
 int main(){
-    char str[100];
+    char str[12];
     cout << "enter char array: ";
-    cin.getline(str,100);
-    cout << "output: "<< str<<endl;
+    cin.getline(str,10);
+    for (char ch:str){
+        cout << ch << " ";
+    }
+    cout << endl;
     return 0;
 }
