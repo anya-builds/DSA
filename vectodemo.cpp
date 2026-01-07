d0e8ea9680ff75eea1ec68a1db84ac1cd92c6140
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-07 19:37:14 +0530

    vector

--- vectodemo.cpp
+++ vectodemo.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(){
     vector<int> vec={1,2,3,4,5};
-   vec.erase(vec.begin()+2);
-    for (int val:vec){
-        cout << val << " ";
+
+    vector<int>::reverse_iterator it;
+    for(it=vec.rbegin();it!=vec.rend();it++){
+        cout << *(it) << " ";
     }
     cout << endl;
-    
 
 
     return 0;
