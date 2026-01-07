b9b90c411adeb88d3951888afe0ac0986893a536
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-07 19:16:16 +0530

    vector

--- vectodemo.cpp
+++ vectodemo.cpp
@@ -4,21 +4,13 @@
 using namespace std;
 
 int main(){
-    vector<int> vec;
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(3);
-    vec.push_back(7);
-    vec.push_back(5);
-    vec.push_back(8);
-    vec.emplace_back(10);
-    vec.pop_back();
+    vector<int> vec={1,2,3,4,5};
+   vec.erase(vec.begin()+2);
     for (int val:vec){
         cout << val << " ";
     }
     cout << endl;
-    cout << "val at idx" << vec[2] << "or " << vec.at(2) << endl;
-    // cout << vec.capacity() << endl;
+    
 
 
     return 0;
