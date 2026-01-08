accaa245f1c16c619ce2051ed00166c78c323def
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-08 18:30:27 +0530

    Queue

---
+++ queuedemo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include<queue>
+#include<stack>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    while (!q.empty())
+    {
+        /* code */
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    
+    return 0;
+}
