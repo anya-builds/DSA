8a7c71889d03017acaa7eab455c714d955f86f44
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-21 22:30:52 +0530

    recursion

--- rec.cpp
+++ rec.cpp
@@ -1,16 +1,16 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// void printNum(int n){
-//     if(n==1){
-//         cout << "1\n" ;
-//         return;
-//     }
+void printNum(int n){
+    if(n==1){
+        cout << "1\n" ;
+        return;
+    }
 
-//     cout << n << " ";
-//     printNum(n-1);
-// }
-// int main(){
-//     printNum(4);
-//     return 0;
-// }
+    cout << n << " ";
+    printNum(n-1);
+}
+int main(){
+    printNum(4);
+    return 0;
+}
